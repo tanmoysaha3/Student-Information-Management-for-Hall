f95f4c8e2b89f88554882c95d92d669f94361df2
Tanmoy <tanmoy3.ts3@gmail.com>
2021-03-13 13:21:24 +0600

    Admin function Add hall,floor,room,seat,search student details

---
+++ app/src/main/java/com/example/simpleapp/AddFloor.java
@@ -0,0 +1,67 @@
+package com.example.simpleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.Timestamp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AddFloor extends AppCompatActivity {
+
+    EditText mEnterHallId, mEnterFloorNo;
+    Button mCreateNewFloorButton;
+    String userID;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    FirebaseUser fUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_floor);
+
+        mEnterHallId=findViewById(R.id.enterHallId);
+        mEnterFloorNo=findViewById(R.id.enterFloorNo);
+        mCreateNewFloorButton=findViewById(R.id.createNewFloorButton);
+
+        fAuth=FirebaseAuth.getInstance();
+        fStore=FirebaseFirestore.getInstance();
+
+        userID=fAuth.getCurrentUser().getUid();
+        fUser=fAuth.getCurrentUser();
+
+        mCreateNewFloorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String hallTd=mEnterHallId.getText().toString();
+                String floorNo=mEnterFloorNo.getText().toString();
+
+                DocumentReference documentReference=fStore.collection("Hall").document(hallTd).collection("Floors").document(floorNo);
+                Map<String,Object> user = new HashMap<>();
+                user.put("Date", Timestamp.now());
+
+                documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d("TAG","onSuccess : user profile is created for " + userID);
+                        startActivity(new Intent(getApplicationContext(),Admin.class));
+                    }
+                });
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/simpleapp/Login.java
+++ app/src/main/java/com/example/simpleapp/Login.java
@@ -8,6 +8,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,7 +22,9 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
 public class Login extends AppCompatActivity {
@@ -30,6 +33,7 @@ public class Login extends AppCompatActivity {
     TextView mRegisterTextButton, mresetPasswordLink;
     ProgressBar mProgressBar;
     FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +45,7 @@ public class Login extends AppCompatActivity {
         mProgressBar=findViewById(R.id.loginProgressBar);
 
         fAuth=FirebaseAuth.getInstance();
+        fStore=FirebaseFirestore.getInstance();
         mLoginButton=findViewById(R.id.loginButton);
         mRegisterTextButton=findViewById((R.id.registerText));
         mresetPasswordLink=findViewById(R.id.resetPasswordLink);
@@ -72,7 +77,20 @@ public class Login extends AppCompatActivity {
 
                 //authenticate the user
 
-                fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                fAuth.signInWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        Toast.makeText(Login.this, "Logged in Successfully.", Toast.LENGTH_SHORT).show();
+                        checkUserAccessLevel(authResult.getUser().getUid());
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(Login.this, "Error", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                /*fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
@@ -84,7 +102,7 @@ public class Login extends AppCompatActivity {
                             mProgressBar.setVisibility((View.GONE));
                         }
                     }
-                });
+                });*/
             }
         });
 
@@ -134,5 +152,41 @@ public class Login extends AppCompatActivity {
                 passwordResetDialog.create().show();
             }
         });
+    }
+    private void checkUserAccessLevel(String uid){
+        DocumentReference documentReference=fStore.collection("users").document(uid);
+        documentReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                Log.d("TAG","onSuccess: " + documentSnapshot.getData());
+                //identify user access level
+                if(documentSnapshot.getString("isAdmin")!=null){
+                    //user is admin
+                    startActivity(new Intent(getApplicationContext(),Admin.class));
+                    finish();
+                }
+
+                else if(documentSnapshot.getString("isSubAdmin")!=null){
+                    //user is SubAdmin
+                    startActivity(new Intent(getApplicationContext(),Admin.class));
+                    finish();
+                }
+
+                else/*(documentSnapshot.getString("isUser")!=null)*/{
+                    //user is user
+                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                    finish();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(FirebaseAuth.getInstance().getCurrentUser()!=null){
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }
     }
 }
