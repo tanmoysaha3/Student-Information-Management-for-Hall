5fe7161765bdc99a79eba4a25abe3030477b85cf
susmita546 <bsusmita546@gmail.com>
2021-04-11 14:03:54 -1200

    HallAdmin

---
+++ app/src/main/java/com/example/simpleapp/HallAdmin.java
@@ -0,0 +1,83 @@
+package com.example.simpleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class HallAdmin extends AppCompatActivity {
+
+    Button mAddHallAdminButton, mSeeAllUser, mCreateHallButton, mCreateFloorButton, mCreateRoomButton,
+            mCreateSeatButton, mSeeStuDetailsButton;
+    String userID;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    FirebaseUser fUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hall_admin);
+
+
+        mCreateHallButton=findViewById(R.id.createHallButton);
+        mCreateFloorButton=findViewById(R.id.createFloorButton);
+        mCreateRoomButton=findViewById(R.id.createRoomButton);
+        mCreateSeatButton=findViewById(R.id.createSeatButton);
+        mSeeStuDetailsButton=findViewById(R.id.seeStuDetailsButton);
+        fAuth=FirebaseAuth.getInstance();
+        fStore=FirebaseFirestore.getInstance();
+
+        userID=fAuth.getCurrentUser().getUid();
+        fUser=fAuth.getCurrentUser();
+
+        mCreateHallButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),AddHall.class));
+            }
+        });
+
+        mCreateFloorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),AddFloor.class));
+            }
+        });
+
+        mCreateRoomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),AddRoom.class));
+            }
+        });
+
+        mCreateSeatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),AddSeat.class));
+            }
+        });
+
+        mSeeStuDetailsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),SeeStuDetails.class));
+            }
+        });
+
+    }
+
+    public void logoutHallAdmin(View view){
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(),Login.class));
+        finish();
+    }
+
+}
